790fc5e766843b7243f0fd637cc7a4936407f4d4
nyxellang <dinoboymohapore123@gmail.com>
2026-06-14 09:04:14 +0300

    add EC2 describe instances script

--- boto3test.py
+++ boto3test.py
@@ -4,14 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-s3 = boto3.client(
-    "s3",
+ec2 = boto3.client(
+    "ec2",
     endpoint_url=os.getenv("‍‍‍‍AWS_ENDPOINT_URL"),
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
     region_name=os.getenv("AWS_DEFAULT_REGION"),
 )
 
-response = s3.list_buckets()
-for buckets in response["Buckets"]:
-    print(buckets["Name"])
+response = ec2.describe_instances()
+for reservations in response["Reservations"]:
+    for instance in reservations["Instances"]:
+        print(instance["InstanceId"])
+        print(instance["InstanceType"])
+        print(instance["State"]["Name"])
