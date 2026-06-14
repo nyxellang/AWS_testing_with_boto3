1acb1735be3ddf60bc4c97dd22b73cf7e8769c89
nyxellang <dinoboymohapore123@gmail.com>
2026-06-14 07:56:30 +0300

    initial commit

---
+++ boto3test.py
@@ -0,0 +1,14 @@
+import boto3
+
+s3 = boto3.client(
+    "s3",
+    endpoint_url="http://localhost:4566",
+    aws_access_key_id="test",
+    aws_secret_access_key="test",
+    region_name="us-east-1",
+)
+
+response = s3.list_buckets()
+
+for buckets in response["Buckets"]:
+    print(buckets["Name"])
