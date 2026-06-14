a8cb03a30a830169343805ed1e7184a34cd3039f
nyxellang <dinoboymohapore123@gmail.com>
2026-06-14 08:29:15 +0300

    add dotenv support, gitignore, readme

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Until now there is only 1 script but it might increase later on.
 ## scripts
 - `boto3test.py` — list all S3 buckets
 yea thats it bye
+
+
+
+
--- boto3test.py
+++ boto3test.py
@@ -1,14 +1,17 @@
 import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 s3 = boto3.client(
     "s3",
-    endpoint_url="http://localhost:4566",
-    aws_access_key_id="test",
-    aws_secret_access_key="test",
-    region_name="us-east-1",
+    endpoint_url=os.getenv("‍‍‍‍AWS_ENDPOINT_URL"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_DEFAULT_REGION"),
 )
 
 response = s3.list_buckets()
-
 for buckets in response["Buckets"]:
     print(buckets["Name"])
